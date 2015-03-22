012bd20b73924a0fad797bb845e326adce29db0e
stla <laurent_step@yahoo.fr>
2015-03-22 13:11:30 +0100

    stacking

--- scripts/coverage_gibbs2.R
+++ scripts/coverage_gibbs2.R
@@ -3,24 +3,25 @@ library(abind)
 library(magrittr)
 
 
-set.seed(1975) #set.seed(2009)
-N<-25  # number of measurments (pour chaque appareil)
-J<-4  # repetitions
+set.seed(666) 
+N<-25  # number of groups
+J<-4  # number of repetitions within each group
 theta0 <- 1:N  # true theta_i
 gammaX0 <- sqrt(4); 
-Lambdas <- c(0.5, 1, 2, 4); l <- length(Lambdas)
+Lambdas <- c(0.5, 1, 2, 4)
 
-nsims <- 200
+nsims <- 1000
 SIMULATIONS <- SUMMARIES <- vector(mode = "list", length = nsims)
 
 
 for(num in 1:nsims){  
-  SUMMARIES[[num]] <- vector(mode = "list", length = l)
-  for(ll in 1:l){ 
-    lambda <- Lambdas[ll]
+  SUMMARIES[[num]] <- vector(mode = "list", length = length(Lambdas))
+  for(l in 1:length(Lambdas)){ 
+    # simulated data
+    lambda <- Lambdas[l]
     gammaY0 <- sqrt(lambda)*gammaX0
     X<-Y<-array(NA,dim=c(N,J))
-    for(i in 1:N){ # simulated data
+    for(i in 1:N){ 
       for(j in 1:J){
         X[i,j] <- rnorm(1,theta0[i],gammaX0)
         Y[i,j] <- rnorm(1,theta0[i],gammaY0)	
@@ -28,19 +29,38 @@ for(num in 1:nsims){
     }
     X <- stack(data.frame(t(X))) %>% setNames(c("x","group"))
     Y <- stack(data.frame(t(Y))) %>% setNames(c("y","group"))
+    # monitored parameters
     params <- c("alpha", "beta", "gamma2X", "kappa2")
-    OUT <- deming_gibbs2(X, Y, nsims=10000, burnin=1000, params=params)
-    SIMS <- lapply(setNames(params,params), function(param) do.call(abind, lapply(OUT, function(out) out[[param]])))
+    # Gibbs sampler
+    OUT <- deming_gibbs2(X, Y, nsims=4000, burnin=500, params=params)
+    # outputs
+    SIMS <- lapply(setNames(params,params), 
+                   function(param) do.call(abind, lapply(OUT, 
+                                                         function(out) out[[param]])))
     alpha.sims <- as.numeric(SIMS[["alpha"]])
     beta.sims <- as.numeric(SIMS[["beta"]])
     means <- sapply(SIMS[params], mean) 
     quantiles <- sapply(SIMS[params], quantile, probs = c(0.025, 0.975)) 
-    posterior.summaries <- round(rbind(means,quantiles),2) 
-    SIMULATIONS[[num]][[ll]] <- list(alpha=alpha.sims,beta=beta.sims)
-    SUMMARIES[[num]][[ll]] <- posterior.summaries
+    posterior.summaries <- rbind(means,quantiles)
+    SIMULATIONS[[num]][[l]] <- list(alpha=alpha.sims,beta=beta.sims)
+    SUMMARIES[[num]][[l]] <- posterior.summaries
   }
 }
 
+# faire aussi un truc qui transforme la matrice theta - a ou plutôt faire ça dans gibbs() ?
+
+head(as.data.table(t(OUT[[1]]$theta)))
+
+theta <- t(OUT[[1]]$theta)
+Theta <- setNames(as.data.table(theta), sprintf(paste0("theta_%0", floor(log10(ncol(theta)))+1, "d"), 1:ncol(theta)))
+
+x <- list(a=11, b=12, c=13)
+Map(function(x, i) paste(i, x), x, seq_along(x))
+
+iterations <- 5:10 # tronquer au préalable ou pas ?.. 
+xx <- rbindlist(
+  Map(function(out, i) as.data.table(out)[, "Chain":=rep(i,.N)], OUT, seq_along(OUT))
+  )[, "Iteration":=seq_len(.N), by=Chain]
 
 save(SIMULATIONS, SUMMARIES, file="SIMULATIONS_COVERAGE2.Rdata")
 
@@ -49,19 +69,19 @@ save(SIMULATIONS, SUMMARIES, file="SIMULATIONS_COVERAGE2.Rdata")
 Covers <- array(NA,dim=c(2,l))
 rownames(Covers) <- c("alpha","beta")
 colnames(Covers) <- Lambdas
-for(ll in 1:l){
+for(l in 1:length(Lambdas)){
   cover <- 0
   for(num in 1:nsims){
-    bounds <- SUMMARIES[[num]][[ll]][2:3,"alpha"]
+    bounds <- SUMMARIES[[num]][[l]][2:3,"alpha"]
     cover <- cover + as.numeric(bounds[1]<0 & 0<bounds[2])
   }
-  Covers["alpha",ll] <- cover/nsims
+  Covers["alpha",l] <- cover/nsims
   cover <- 0
   for(num in 1:nsims){
-    bounds <- SUMMARIES[[num]][[ll]][2:3,"beta"]
+    bounds <- SUMMARIES[[num]][[l]][2:3,"beta"]
     cover <- cover + as.numeric(bounds[1]<1 & 1<bounds[2])
   }
-  Covers["beta",ll] <- cover/nsims
+  Covers["beta",l] <- cover/nsims
 }
 
 Covers
@@ -69,9 +89,9 @@ Covers
 
 Alphas <- array(NA,dim=c(nsims,l)) -> Betas
 colnames(Alphas) <- colnames(Betas) <- Lambdas
-for(ll in 1:l){
+for(l in 1:length(Lambdas)){
   for(num in 1:nsims){
-    Alphas[num,ll] <- SUMMARIES[[num]][[ll]]["means","alpha"]
-    Betas[num,ll] <- SUMMARIES[[num]][[ll]]["means","beta"]
+    Alphas[num,l] <- SUMMARIES[[num]][[l]]["means","alpha"]
+    Betas[num,l] <- SUMMARIES[[num]][[l]]["means","beta"]
   }
 }
